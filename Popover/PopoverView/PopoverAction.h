acbf832806ca10625fd60c0eaa0c2c917b00b2a0
Mohamed Afifi <mohamede1945@users.noreply.github.com>
2017-10-09 00:24:03 -0400

    Update PopoverAction.h

--- Popover/PopoverView/PopoverAction.h
+++ Popover/PopoverView/PopoverAction.h
@@ -1,4 +1,10 @@
-
+//
+//  PopoverAction.h
+//  Popover
+//
+//  Created by StevenLee on 2016/12/10.
+//  Copyright © 2016年 lifution. All rights reserved.
+//
 
 #import <Foundation/Foundation.h>
 #import <UIKit/UIKit.h>
@@ -10,12 +16,13 @@ typedef NS_ENUM(NSUInteger, PopoverViewStyle) {
 
 @interface PopoverAction : NSObject
 
-@property (nonatomic, strong, readonly) UIImage *image; ///< 图标 (建议使用 60pix*60pix 的图片)
-@property (nonatomic, copy, readonly) NSString *title; ///< 标题
-@property (nonatomic, copy, readonly) void(^handler)(PopoverAction *action); ///< 选择回调, 该Block不会导致内存泄露, Block内代码无需刻意去设置弱引用.
+@property (nonatomic, strong, readonly) UIImage * _Nullable image; ///< 图标 (建议使用 60pix*60pix 的图片)
+@property (nonatomic, copy, readonly) NSString * _Nullable title; ///< 标题
+@property (nonatomic, copy, readonly) void(^ _Nullable handler)(PopoverAction * _Nullable action); ///< 选择回调, 该Block不会导致内存泄露, Block内代码无需刻意去设置弱引用.
 
-+ (instancetype)actionWithTitle:(NSString *)title handler:(void (^)(PopoverAction *action))handler;
 
-+ (instancetype)actionWithImage:(UIImage *)image title:(NSString *)title handler:(void (^)(PopoverAction *action))handler;
++ (instancetype _Nonnull)actionWithTitle:(NSString *_Nullable)title handler:(void (^ _Nullable)(PopoverAction * _Nullable action))handler;
+
++ (instancetype _Nonnull)actionWithImage:(UIImage *_Nullable)image title:(NSString *_Nullable)title handler:(void (^ _Nullable)(PopoverAction * _Nullable action))handler;
 
 @end
